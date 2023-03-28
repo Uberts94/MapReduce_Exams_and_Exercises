462623bda4c2faa37392176d3421907f3c172370
Uberts94 <s292522@studenti.polito.it>
2023-03-28 16:48:49 +0200

    fixing warnings

--- Es18/src/main/java/it/polito/bigdata/hadoop/DriverBigData.java
+++ Es18/src/main/java/it/polito/bigdata/hadoop/DriverBigData.java
@@ -3,7 +3,6 @@ package it.polito.bigdata.hadoop;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
