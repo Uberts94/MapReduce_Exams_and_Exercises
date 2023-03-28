3e635416436467dd85c4b7cb9e6cbfa805bfc930
Uberts94 <s292522@studenti.polito.it>
2023-03-28 16:34:40 +0200

    finished and locally tested

---
+++ Es18/src/main/java/it/polito/bigdata/hadoop/MapperBigData.java
@@ -0,0 +1,35 @@
+package it.polito.bigdata.hadoop;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+/**
+ * Es18 - filtering data
+ */
+class MapperBigData extends Mapper<
+                    LongWritable, // Input key type
+                    Text,         // Input value type
+                    NullWritable,         // Output key type
+                    Text> {// Output value type
+	float threshold;
+	
+	protected void setup(Context context) {
+		threshold = context.getConfiguration().getFloat("threshold", 0);
+	}
+    
+    protected void map(
+            LongWritable key,   // Input key type
+            Text value,         // Input value type
+            Context context) throws IOException, InterruptedException {
+    	
+    	String[] measurement = value.toString().split(",");
+    	float temperature = Float.parseFloat(measurement[3]);
+    	if(temperature > threshold) context.write(NullWritable.get(), new Text(value));
+    		
+    }
+}
