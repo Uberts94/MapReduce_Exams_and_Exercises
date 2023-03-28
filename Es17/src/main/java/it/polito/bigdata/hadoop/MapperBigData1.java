a746a50edbe119f8ba4c63b9d0690b6212c4c69a
Uberts94 <s292522@studenti.polito.it>
2023-03-28 15:53:21 +0200

    setting mappers

--- Es17/src/main/java/it/polito/bigdata/hadoop/MapperBigData1.java
+++ Es17/src/main/java/it/polito/bigdata/hadoop/MapperBigData1.java
@@ -2,39 +2,30 @@ package it.polito.bigdata.hadoop;
 
 import java.io.IOException;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-
 /**
- * Basic MapReduce Project - Mapper
+ * Es17 - mapping data of second file
  */
 class MapperBigData1 extends Mapper<
                     LongWritable, // Input key type
                     Text,         // Input value type
                     Text,         // Output key type
-                    IntWritable> {// Output value type
+                    TemperatureWritable> {// Output value type
     
     protected void map(
             LongWritable key,   // Input key type
             Text value,         // Input value type
             Context context) throws IOException, InterruptedException {
 
-            // Split each sentence in words. Use whitespace(s) as delimiter 
-    		// (=a space, a tab, a line break, or a form feed)
-    		// The split method returns an array of strings
-            String[] words = value.toString().split("\\s+");
+            String[] measurement = value.toString().split("\\,");
             
-            // Iterate over the set of words
-            for(String word : words) {
-            	// Transform word case
-                String cleanedWord = word.toLowerCase();
-                
-                // emit the pair (word, 1)
-                context.write(new Text(cleanedWord),
-                		      new IntWritable(1));
-            }
+            TemperatureWritable t = new TemperatureWritable();
+            t.setDate(measurement[0]);
+            t.setTemperature(Float.parseFloat(measurement[2]));
+            
+            context.write(new Text(measurement[3]), t);
     }
 }
