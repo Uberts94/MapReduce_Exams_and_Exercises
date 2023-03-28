579916010518f6c27ecee4336fd9d10324fbd9c2
Uberts94 <s292522@studenti.polito.it>
2023-03-28 15:46:59 +0200

    setting DriverBigData, adding MapperBigData1.class

--- Es17/src/main/java/it/polito/bigdata/hadoop/DriverBigData.java
+++ Es17/src/main/java/it/polito/bigdata/hadoop/DriverBigData.java
@@ -3,10 +3,9 @@ package it.polito.bigdata.hadoop;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
@@ -23,7 +22,8 @@ implements Tool {
   @Override
   public int run(String[] args) throws Exception {
 
-    Path inputPath;
+    Path inputPath1;
+    Path inputPath2;
     Path outputDir;
     int numberOfReducers;
 	int exitCode;  
@@ -32,9 +32,11 @@ implements Tool {
 	// Number of instances of the reducer class 
     numberOfReducers = Integer.parseInt(args[0]);
     // Folder containing the input data
-    inputPath = new Path(args[1]);
+    inputPath1 = new Path(args[1]);
+ // Folder containing the input data
+    inputPath2 = new Path(args[2]);
     // Output folder
-    outputDir = new Path(args[2]);
+    outputDir = new Path(args[3]);
     
     Configuration conf = this.getConf();
 
@@ -45,7 +47,9 @@ implements Tool {
     job.setJobName("Basic MapReduce Project - WordCount example");
     
     // Set path of the input file/folder (if it is a folder, the job reads all the files in the specified folder) for this job
-    FileInputFormat.addInputPath(job, inputPath);
+    MultipleInputs.addInputPath(job, inputPath1, TextInputFormat.class, MapperBigData.class);
+    
+    MultipleInputs.addInputPath(job, inputPath2, TextInputFormat.class, MapperBigData1.class);
     
     // Set path of the output folder for this job
     FileOutputFormat.setOutputPath(job, outputDir);
@@ -65,14 +69,21 @@ implements Tool {
     
     // Set map output key and value classes
     job.setMapOutputKeyClass(Text.class);
-    job.setMapOutputValueClass(IntWritable.class);
+    job.setMapOutputValueClass(TemperatureWritable.class);
+    
+    // Set map class
+    job.setMapperClass(MapperBigData1.class);
+    
+    // Set map output key and value classes
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(TemperatureWritable.class);
     
     // Set reduce class
     job.setReducerClass(ReducerBigData.class);
         
     // Set reduce output key and value classes
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+    job.setOutputValueClass(Text.class);
 
     // Set number of reducers
     job.setNumReduceTasks(numberOfReducers);
