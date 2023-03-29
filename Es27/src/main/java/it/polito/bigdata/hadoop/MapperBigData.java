e4c56c4e250a12fd0f01bf01c3197900e7731605
Uberts94 <s292522@studenti.polito.it>
2023-03-29 16:27:05 +0200

    finished and locally tested

--- Es27/src/main/java/it/polito/bigdata/hadoop/MapperBigData.java
+++ Es27/src/main/java/it/polito/bigdata/hadoop/MapperBigData.java
@@ -1,12 +1,17 @@
 package it.polito.bigdata.hadoop;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
+import java.util.HashSet;
 
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-
 
 /**
  * Es27 - mapping data
@@ -14,27 +19,42 @@ import org.apache.hadoop.mapreduce.Mapper;
 class MapperBigData extends Mapper<
                     LongWritable, // Input key type
                     Text,         // Input value type
-                    Text,         // Output key type
-                    IntWritable> {// Output value type
-    
+                    NullWritable,         // Output key type
+                    Text> {// Output value type
+	
+	HashSet <String> rules;
+	
+	protected void setup(Context context) throws IOException {
+		String line;
+		URI[] urisCachedFiles = context.getCacheFiles();
+		rules = new HashSet<String>();
+		BufferedReader file = new BufferedReader(new FileReader(new File(new Path(urisCachedFiles[0].getPath()).getName())));
+		
+		while((line = file.readLine())!= null) {
+			rules.add(line);
+		}
+	}
+	
     protected void map(
             LongWritable key,   // Input key type
             Text value,         // Input value type
             Context context) throws IOException, InterruptedException {
-
-            // Split each sentence in words. Use whitespace(s) as delimiter 
-    		// (=a space, a tab, a line break, or a form feed)
-    		// The split method returns an array of strings
-            String[] words = value.toString().split("\\s+");
-            
-            // Iterate over the set of words
-            for(String word : words) {
-            	// Transform word case
-                String cleanedWord = word.toLowerCase();
-                
-                // emit the pair (word, 1)
-                context.write(new Text(cleanedWord),
-                		      new IntWritable(1));
-            }
+    	
+    	String[] userParams = value.toString().split(",");
+    	String category = new String("Unknown");
+    	
+    	for(String rule : rules) {
+    		String[] entry = rule.split("\\s+->\\s+");
+    		String[] ruleParams = entry[0].split("\\s+and\\s+");
+    		String gender = new String(ruleParams[0].replace("Gender=", ""));
+    		int year = Integer.parseInt(ruleParams[1].replace("YearOfBirth=", ""));
+    		
+    		if(userParams[3].equals(gender) && Integer.parseInt(userParams[4]) == (year)) {
+    			category = new String(entry[1]);
+    			break;
+    		}
+    	}
+    	
+    	context.write(NullWritable.get(), new Text(value.toString()+","+category));	
     }
 }
