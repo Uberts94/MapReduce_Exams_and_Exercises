827e311f092403b6d14b96655451ceead0d52696
Uberts94 <s292522@studenti.polito.it>
2023-03-29 13:55:29 +0200

    finished and locally tested

---
+++ Es24/src/main/java/it/polito/bigdata/hadoop/ReducerBigData.java
@@ -0,0 +1,31 @@
+package it.polito.bigdata.hadoop;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ * Basic MapReduce Project - Reducer
+ */
+class ReducerBigData extends Reducer<
+                Text,           // Input key type
+                Text,    // Input value type
+                NullWritable,           // Output key type
+                Text> {  // Output value type
+    
+    @Override
+    
+    protected void reduce(
+        Text key, // Input key type
+        Iterable<Text> values, // Input value type
+        Context context) throws IOException, InterruptedException {
+
+        String friends = new String(key.toString()+": ");
+        
+        for(Text friend : values) friends += friend.toString()+" ";
+        
+        context.write(NullWritable.get(), new Text(friends));
+    }
+}
