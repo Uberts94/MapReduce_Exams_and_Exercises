40961027cbc073bd9b6bfaae5b66f870eba35eac
Uberts94 <s292522@studenti.polito.it>
2023-03-29 14:01:52 +0200

    adding base project, starting from Es24

---
+++ Es25/src/main/java/it/polito/bigdata/hadoop/Mapper1BigData.java
@@ -0,0 +1,30 @@
+package it.polito.bigdata.hadoop;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+/**
+ * Es24 - mapping data
+ */
+class Mapper1BigData extends Mapper<
+                    LongWritable, // Input key type
+                    Text,         // Input value type
+                    Text,         // Output key type
+                    Text> {// Output value type
+    
+    protected void map(
+            LongWritable key,   // Input key type
+            Text value,         // Input value type
+            Context context) throws IOException, InterruptedException {
+
+            String[] pair = value.toString().split(",");
+            
+            context.write(new Text(pair[0]), new Text(pair[1]));
+            context.write(new Text(pair[1]), new Text(pair[0]));
+
+    }
+}
