703d0a6f15b9ee89b3791cbc0f6a5fd7669b90e5
Uberts94 <s292522@studenti.polito.it>
2023-03-29 13:30:30 +0200

    finished and locally tested

--- Es23Bis/src/main/java/it/polito/bigdata/hadoop/Reducer1BigData.java
+++ Es23Bis/src/main/java/it/polito/bigdata/hadoop/Reducer1BigData.java
@@ -1,7 +1,13 @@
 package it.polito.bigdata.hadoop;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
+import java.util.HashSet;
 
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -16,18 +22,28 @@ class Reducer1BigData extends Reducer<
                 NullWritable> {  // Output value type
 	
 	String potentialFriends;
+	HashSet <String> friends;
 	
-	protected void setup(Context context) {
+	protected void setup(Context context) throws IOException {
+		String line;
+		URI[] urisCachedFiles = context.getCacheFiles();
+		friends = new HashSet<String>();
 		potentialFriends = new String();
+		
+		BufferedReader file = new BufferedReader(new FileReader(new File(new Path(urisCachedFiles[0].getPath()).getName())));
+		
+		while((line = file.readLine())!= null) {
+			friends.add(line);
+		}
 	}
-    
+	
     @Override
     protected void reduce(
         Text key, // Input key type
         Iterable<NullWritable> values, // Input value type
         Context context) throws IOException, InterruptedException {
     	
-    	potentialFriends += key.toString()+ " ";
+    	if(!friends.contains(key.toString())) potentialFriends += key.toString()+ " ";
     }
     
     protected void cleanup(Context context) throws IOException, InterruptedException {
