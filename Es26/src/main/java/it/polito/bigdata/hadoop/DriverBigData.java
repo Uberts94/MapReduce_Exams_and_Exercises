f48407947a197ef8ec88520db2dbaf93b65158c1
Uberts94 <s292522@studenti.polito.it>
2023-03-29 14:48:24 +0200

    finished and locally tested

--- Es26/src/main/java/it/polito/bigdata/hadoop/DriverBigData.java
+++ Es26/src/main/java/it/polito/bigdata/hadoop/DriverBigData.java
@@ -3,7 +3,7 @@ package it.polito.bigdata.hadoop;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -25,21 +25,21 @@ implements Tool {
 
     Path inputPath;
     Path outputDir;
-    int numberOfReducers;
 	int exitCode;  
 	
 	// Parse the parameters
-	// Number of instances of the reducer class 
-    numberOfReducers = Integer.parseInt(args[0]);
     // Folder containing the input data
-    inputPath = new Path(args[1]);
+    inputPath = new Path(args[0]);
     // Output folder
-    outputDir = new Path(args[2]);
+    outputDir = new Path(args[1]);
     
     Configuration conf = this.getConf();
 
     // Define a new job
     Job job = Job.getInstance(conf); 
+    
+    // Set cache file path
+    job.addCacheFile(new Path("dictionary.txt").toUri());
 
     // Assign a name to the job
     job.setJobName("Es26");
@@ -64,18 +64,11 @@ implements Tool {
     job.setMapperClass(MapperBigData.class);
     
     // Set map output key and value classes
-    job.setMapOutputKeyClass(Text.class);
-    job.setMapOutputValueClass(IntWritable.class);
+    job.setMapOutputKeyClass(NullWritable.class);
+    job.setMapOutputValueClass(Text.class);
     
-    // Set reduce class
-    job.setReducerClass(ReducerBigData.class);
-        
-    // Set reduce output key and value classes
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-
     // Set number of reducers
-    job.setNumReduceTasks(numberOfReducers);
+    job.setNumReduceTasks(0);
     
     
     // Execute the job and wait for completion
--- Es26/src/main/java/it/polito/bigdata/hadoop/MapperBigData.java
+++ Es26/src/main/java/it/polito/bigdata/hadoop/MapperBigData.java
@@ -1,9 +1,15 @@
 package it.polito.bigdata.hadoop;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
 
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
@@ -14,27 +20,35 @@ import org.apache.hadoop.mapreduce.Mapper;
 class MapperBigData extends Mapper<
                     LongWritable, // Input key type
                     Text,         // Input value type
-                    Text,         // Output key type
-                    IntWritable> {// Output value type
+                    NullWritable,         // Output key type
+                    Text> {// Output value type
+	HashMap <String, Integer> words;
+	
+	protected void setup(Context context) throws IOException {
+		String line;
+		URI[] urisCachedFiles = context.getCacheFiles();
+		words = new HashMap<String, Integer>();
+		
+		BufferedReader file = new BufferedReader(new FileReader(new File(new Path(urisCachedFiles[0].getPath()).getName())));
+		
+		while((line = file.readLine())!= null) {
+			String[] entry = line.split("\t+");
+			words.put(entry[1], Integer.parseInt(entry[0]));
+		}
+	}
     
     protected void map(
             LongWritable key,   // Input key type
             Text value,         // Input value type
             Context context) throws IOException, InterruptedException {
 
-            // Split each sentence in words. Use whitespace(s) as delimiter 
-    		// (=a space, a tab, a line break, or a form feed)
-    		// The split method returns an array of strings
-            String[] words = value.toString().split("\\s+");
+            String[] text = value.toString().split("\\s+");
+            String line = new String();
             
-            // Iterate over the set of words
-            for(String word : words) {
-            	// Transform word case
-                String cleanedWord = word.toLowerCase();
-                
-                // emit the pair (word, 1)
-                context.write(new Text(cleanedWord),
-                		      new IntWritable(1));
-            }
+            for(String w : text) 
+            	if(words.containsKey(w)) line += words.get(w)+" ";
+            	else line += w+" ";
+            
+            context.write(NullWritable.get(), new Text(line));
     }
 }
